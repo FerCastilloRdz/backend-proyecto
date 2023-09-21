322d578bc8d9cc14207a4376c597d97a81c4db84
Fernando <fer.castillo.rdz999@gmail.com>
2023-09-20 20:06:15 -0600

    Proyecto Completo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import express from 'express'
+import express  from 'express'
 import cors from 'cors'
-import { initializeApp } from 'firebase/app'
-import { getFirestore, collection, getDocs, addDoc, doc, deleteDoc, getDoc, updateDoc} from "firebase/firestore"
+import {initializeApp} from 'firebase/app'
+import { getFirestore, collection, getDocs, setDoc, addDoc, deleteDoc, doc, getDoc, updateDoc} from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBEmXOEMUZ4nMAabh328dZQVsbES-XR20g",
@@ -15,116 +15,104 @@ const firebaseConfig = {
   const firebase = initializeApp(firebaseConfig)
   const db = getFirestore(firebase)
 
-  // Settings App
-const app = express();
-app.use(express.json());
-app.use(cors());
+//settings de la aplicacion
+const app = express()
+app.use(express.json())
+app.use(cors())
 
-// Routes
+//creacion de rutas
 app.get('/', async (req, res) => {
-    try {
-        const Users = await collection(db, 'Users')
-        const listUsers = await getDocs(Users)
-        const aux = []
-        listUsers.forEach((doc) => {
-            const obj = {
-            id: doc.id,
-            ...doc.data()
-        }
-        aux.push(obj)
+  try{
+    const Users = await collection(db, 'Users')
+    const listUsers = await getDocs(Users)
+    const aux = []
+    listUsers.forEach((doc) => {
+      const obj = {
+        id: doc.id,
+      ...doc.data()
+      }
+       aux.push(obj)
     })
-        res.send({
-            'msg': 'success',
-            'data': aux
-        })
-    } catch(error) {
-        res.send({
-            'msg': 'error',
-            'data': error
-        })  
-    }
-})
-
-app.post('/create', async(req, res) => {
-    try{
-        const body = req.body;
-        const Users = await collection(db, 'Users');
-        await addDoc(Users, body);
-        res.send({
-            'msg': 'success'
-        })
-    } catch (error) {
-        res.send({
-            'msg': 'error',
-            'data': 'error'
-        })
-    }
-
-})
-
-app.get('/get-update/:id', async (req, res) => {
-    const id = req.params.id;
-
-    const userRef = doc(db, 'Users',id);
-    const user = await getDoc(userRef);
-    //console.log(user.exists());
-
-    if (user.exists()){
-        res.send({
-            'msg': 'success',
-            'data': user.data()
-        })
-    }
-    else {
-        res.send({
-            'msg': 'user doent exist'
-        })
-    }
-})
-
-app.post('/update', async (req, res) => {
-    const {id, firstname, lastname, address, city, phone, zipcode} = req.body;
-    const newData = {
-        firstname,
-        lastname,
-        address,
-        city,
-        phone,
-        zipcode
-    }
-
-    try {
-        await updateDoc(doc(db, "Users", id), newData)
     res.send({
-        'msg': 'success'
+        'msg': 'success',
+        'data': aux
     })
-    } catch (error) {
-        console.log(error)
-    }
-    
-    console.log("@@@ body => ", req.body);
+  }catch(error){
+    res.send({
+      'msg': 'error',
+      'data': aux
+  })
+  }
+
+})
+
+app.post('/create', async (req, res) => {
+  try{
+    const body = req.body
+    const Users = await collection(db, "Users")
+    await addDoc(Users, body)
+    res.send({
+      'msg': 'success'
+    })
+  } catch(error) {
+      res.send({
+        'msg': 'error',
+        'data': error
+      })
+  }
 })
 
 app.get('/delete/:id', async (req, res) => {
-    //console.log('@@@ param => ', req.params.id);
-    const id = req.params.id;
-    try{
-        await deleteDoc(doc(db, 'Users', id));
-        res.send({
-            'msg': 'user deleted'
-        })
-    }
-    catch (error){
-        res.send({
-            'msg': 'error',
-            'data': 'error'
-        })
-    }
-});
+  console.log('$$ param => ', req.params.id)
+  const id = req.params.id
+  try{
+    await deleteDoc(doc(db, 'Users', id))
+    res.send({
+      'msg': 'user deleted'
+    })
+  }catch (error) {
+    res.send({
+      'msg': 'error',
+      'data': error
+    })
+  }
+})
 
-// Run Server
-const port = process.env.PORT || 5000;
+app.get('/get-update/:id', async (req, res) => {
+  const id = req.params.id
 
-app.listen(port, () => {
-    console.log(`Escuchando en el puerto ${port}...`);
-});
+  const userRef = doc(db, 'Users', id)
+  const user = await getDoc(userRef)
+
+  if (user.exists()) {
+    res.send({
+      'msg': 'success',
+      'data': user.data()
+    })
+  } else {
+    res.send({
+      'msg': 'user doesnt exists'
+    })
+  }
+})
+
+app.post('/update', async (req, res) => {
+  const {id, firstname, lastname, address, phone, city, cp} = req.body
+  const newData = {
+    firstname,
+    lastname,
+    address,
+    phone,
+    city,
+    cp
+  }
+  await updateDoc(doc(db, 'Users', id), newData)
+  res.send({
+    'msg': 'success'
+  })
+  //console.log('$$ body => ', result)
+})
+//prendemos el servidor
+app.listen(9000, () => {
+    console.log('servidor trabajando')
+})
